edee57f9770d45bcf7ba7d8684e7b047b8fd0e44
leovingo <ngpthanh1722@gmail.com>
2023-12-31 18:27:11 +0700

    update new ext

--- Extension/translate.js
+++ Extension/translate.js
@@ -1,80 +1,24 @@
 console.log("translate.js loaded", chrome);
 const bodyDOM = document.querySelector("body");
-
-function getSelectedText() {
-    let selectedText = "";
-
-    // window.getSelection
-    if (window.getSelection) {
-        selectedText = window.getSelection().toString();
-    }
-    // document.getSelection for Firefox
-    else if (document.getSelection) {
-        selectedText = document.getSelection().toString();
-    }
-    // document.selection 
-    else if (document.selection) {
-        selectedText =
-            document.selection.createRange().text;
-    } else return "";
-    return selectedText
-}
-function getSelectedTextNode() {
-    let selectedText = "";
-
-    // window.getSelection
-    if (window.getSelection) {
-        selectedText = window.getSelection();
-    }
-    // document.getSelection for Firefox
-    else if (document.getSelection) {
-        selectedText = document.getSelection();
-    }
-    // document.selection 
-    else if (document.selection) {
-        selectedText =
-            document.selection.createRange();
-    } else return "";
-    return selectedText
-}
-
-function renderTooltipIcon(selectionTextRange) {
+function renderTooltipIcon() {
     const tooltipWrapper = document.createElement("div");
     tooltipWrapper.id = "translate-extension";
-    const tooltipIcon = document.createElement("div");  
+    const tooltipIcon = document.createElement("div");
     tooltipIcon.classList.add("translate-extension-icon");
-    tooltipIcon.style.backgroundImage = "url('icon.png')";
-    tooltipWrapper.appendChild(tooltipIcon);
     
-    const top  = selectionTextRange.top + selectionTextRange.height + 6 + "px";
-    const left = selectionTextRange.left + ( selectionTextRange.width / 2 - tooltipWrapper.offsetWidth / 2) + "px";
+    tooltipWrapper.appendChild(tooltipIcon);
 
-    tooltipWrapper.style.position = 'absolute';
+    // Set the position to be fixed and adjust right to center it on the right side
+    tooltipWrapper.style.position = 'fixed';
     tooltipWrapper.style.background = "blue";
     tooltipWrapper.style.padding = "10px";
-    tooltipWrapper.style.top = top;
-    tooltipWrapper.style.left = left;
-
-    bodyDOM.appendChild(tooltipWrapper)
+    tooltipWrapper.style.top = "50%";
+    tooltipWrapper.style.right = "0%";  // Adjusted to be 20% from the right side
+    tooltipWrapper.style.transform = "translate(0, -50%)";  // Adjust for centering
+    bodyDOM.appendChild(tooltipWrapper);
 }
 
-function getRangeSectionText(){
-    const selectedTextNode = getSelectedTextNode();
-        const getRange = selectedTextNode.getRangeAt(0);
-        const selectionRect = getRange.getBoundingClientRect();
-        return selectionRect;
-}
-bodyDOM.addEventListener("mouseup", () => {
-    const selectedText = getSelectedText();
-    if(selectedText && selectedText.length > 0){
-        const selectionTextRange = getRangeSectionText();
-        renderTooltipIcon(selectionTextRange);
-
-        setTimeout(() => {
-            const tooltipWrapper = document.querySelector("#translate-extension");
-            if(tooltipWrapper) tooltipWrapper.remove();
-        }, 3000);
-    }
-    
-});
+window.onload = () => {
+    renderTooltipIcon();
+};
 
