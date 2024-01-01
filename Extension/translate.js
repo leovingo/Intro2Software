19959608378a303c5add4491e3d5665a6a5921b6
leovingo <ngpthanh1722@gmail.com>
2024-01-01 10:37:51 +0700

    update new ext - gptq

--- Extension/translate.js
+++ Extension/translate.js
@@ -15,10 +15,23 @@ function renderTooltipIcon() {
     tooltipWrapper.style.top = "50%";
     tooltipWrapper.style.right = "0%";  // Adjusted to be 20% from the right side
     tooltipWrapper.style.transform = "translate(0, -50%)";  // Adjust for centering
+    tooltipWrapper.addEventListener("click", () => {
+        window.open("http://127.0.0.1:5500/Extension/popup/popup.html", "extension_popup", "width=500,height=500,status=no,scrollbars=yes,resizable=no");
+    });
+    // tooltipWrapper.addEventListener("click", () => {
+    //     console.log("click")
+    //     chrome.tabs.create({
+    //         url: "http://127.0.0.1:5500/Extension/popup/popup.html",
+    //         type: "popup",
+    //         width: 500,
+    //         height: 500,
+    //         focused: true,
+    //         // Nếu bạn muốn điều chỉnh các thuộc tính khác của cửa sổ, bạn có thể thêm chúng ở đây
+    //     });
+    // });
     bodyDOM.appendChild(tooltipWrapper);
 }
 
 window.onload = () => {
     renderTooltipIcon();
 };
-
